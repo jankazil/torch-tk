22a0c1ee680a44b3f9436e9de39c288315a9a1d6
Jan Kazil <jan.kazil.dev@gmail.com>
2026-04-19 18:14:37 -0600

    Fix

--- src/torch_tk/diagnostics/__init__.py
+++ src/torch_tk/diagnostics/__init__.py
@@ -1,3 +1,3 @@
 from .diagnostics import Diagnostics
 from .loss import model_worst_loss, per_sample_loss_from_data, per_sample_loss_from_data_loader
-from .plotting import plot_diagnostics
+from .plotting import plot_positive_loss_kde_pdf
